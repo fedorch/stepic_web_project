9a38bbdd2622fd7857f5f32d5f405a7bd43b5e1b
Vladimir Fedorchuk <vfedorchuk@gmail.com>
2016-05-06 20:00:24 +0400

    Task 1.9.11 attempt 2

--- init.sh
+++ init.sh
@@ -2,3 +2,4 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
