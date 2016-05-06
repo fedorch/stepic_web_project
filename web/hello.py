01d4335d5bee1c18659f3f0c6e4ea2f0b1ec4cc1
Vladimir Fedorchuk <vfedorchuk@gmail.com>
2016-05-06 20:27:09 +0400

    Task 1.9.11 attempt 3

--- web/hello.py
+++ web/hello.py
@@ -1,3 +1,3 @@
-def app(env, start_response):
+def app(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    return  ['Hello, world!\r\n', environ['QUERY_STRING']]
+    return ['\r\n'.join(environ['QUERY_STRING'].split('&'))]
